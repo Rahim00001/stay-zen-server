b792a4e072784bb6d397a58b07e824ca9f464084
Rahim00001 <rahimlpt2022@gmail.com>
2023-11-07 18:49:08 +0600

    rooms data added to database

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 console.log(process.env.DB_PASS);
 console.log(process.env.DB_USER);
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lk92epi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +27,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const roomCollection = client.db('stayZen').collection('rooms');
+
+        app.get('/rooms', async (req, res) => {
+            const cursor = roomCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
